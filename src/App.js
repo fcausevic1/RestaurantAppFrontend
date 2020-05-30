cf6d8b8a756219e03eef250b33b729922393437c
Faris135 <fcausevic1@etf.unsa.ba>
2020-05-30 15:04:12 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import ProductList from './components/ProductList.jsx';
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
+import SideBar from "./components/SideBar";
 
 class App extends Component {
-  render() {
+  render() {  
     return (
       <div>
-     
-        <NavBar></NavBar>
-        <ProductList></ProductList>
-      </div>
+      <NavBar></NavBar>
+        <div>
+          <ProductList></ProductList>
+          <ProductList></ProductList>
+          <ProductList></ProductList>
+          <SideBar></SideBar>
+        </div>
+  
+    </div>
     );
-  }
+  } 
 }
 
 export default App;
