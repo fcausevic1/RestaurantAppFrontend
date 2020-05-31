250d5586ef706acfcaf75640ddf6c193df6aaded
dejaan <dpopovic1@etf.unsa.ba>
2020-05-31 06:12:48 +0200

    Removing items from order list fixed and implemented with callback function

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,32 +2,23 @@ import React, { useState } from "react";
 import "./SideBar.css";
 import Proizvod from "./Product";
 
-const removeItem = () => {
-/* Get all elements with class="close" */
-var closebtns = document.getElementsByClassName("close");
-var i;
-
-/* Loop through the elements, and hide the parent, when clicked on */
-for (i = 0; i < closebtns.length; i++) {
-   
-  closebtns[i].parentElement.style.display = 'none';
-};
-}
-
-
-
-
-
-
 const SideBar = (props) => {
   const { orderedProducts } = props;
   return (
     <div className="sideBar">
-      <h1>Narudzba</h1>
+      <h1>Order list</h1>
       <ul className="OrderList">
         {orderedProducts.map((product) => (
           <li className="orderItem" key={product.product_name}>
-            {product.product_name} {product.product_price} <span className="close" onClick={removeItem} >x</span>
+            {product.product_name} {product.product_price}{" "}
+            <span
+              className="close"
+              onClick={() => {
+                props.removeItemFromList(product);
+              }}
+            >
+              x
+            </span>
           </li>
         ))}
       </ul>
@@ -35,4 +26,4 @@ const SideBar = (props) => {
   );
 };
 
-export default SideBar; 
+export default SideBar;
