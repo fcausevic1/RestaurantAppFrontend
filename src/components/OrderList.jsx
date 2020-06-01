563496b777cfd9f47b635b3e4c8f4f792d206028
Faris135 <fcausevic1@etf.unsa.ba>
2020-06-01 17:45:48 +0200

    Orders to represent in a order list

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Order from "./Order.jsx";
 
 const OrderList = (props) => {
+  const { orders } = props;
+
   return (
     <tbody>
-      <Order></Order>
-      <Order></Order>
+      {" "}
+      {orders.map((order) => (
+        <Order
+          key={order.id}
+          id={order.id}
+          date={order.date}
+          completion={order.completion}
+          user={order.user}
+          coupon={order.coupon}
+          items={order.items}
+        ></Order>
+      ))}
     </tbody>
   );
 };
