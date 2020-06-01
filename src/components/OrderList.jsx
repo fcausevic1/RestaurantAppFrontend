659e0ea42459b31af37d3b34a5f5cd23717806db
dejaan <dpopovic1@etf.unsa.ba>
2020-06-01 20:10:04 +0200

    Sidebar bug fixed by changing table to list for orders

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -7,28 +7,26 @@ const OrderList = (props) => {
 
   return (
     <div className="tableContainer">
-      <table className="table">
-        <tbody>
-          {orders.map((order) => (
-            <tr
-              key={order.id}
-              onClick={() => {
-                props.setSelectedOrder(order);
-              }}
-            >
-              <Order
-                setSelectedOrder
-                id={order.id}
-                date={order.date}
-                completion={order.completion}
-                user={order.user}
-                coupon={order.coupon}
-                items={order.items}
-              ></Order>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ul className="table">
+        {orders.map((order) => (
+          <li
+            key={order.id}
+            onClick={() => {
+              props.setSelectedOrder(order);
+            }}
+          >
+            <Order
+              setSelectedOrder
+              id={order.id}
+              date={order.date}
+              completion={order.completion}
+              user={order.user}
+              coupon={order.coupon}
+              items={order.items}
+            ></Order>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
