890dafb919a2a9058e6583e925c109c3b110b5f6
dejaan <dpopovic1@etf.unsa.ba>
2020-06-01 17:44:11 +0200

    Edit product functionality fully implemented

--- src/components/ProductsService.js
+++ src/components/ProductsService.js
@@ -1,11 +1,8 @@
 const ProductsService = (() => {
   const getProducts = async () => {
     const products = await fetch("/api/products").then((response) => {
-      console.log(response);
       return response.json();
     });
-
-    console.log(products);
 
     return products;
   };
