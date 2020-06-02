8afcc863c58f743f0c68a8f1fd438bc9eac5191b
Faris135 <fcausevic1@etf.unsa.ba>
2020-06-02 15:41:29 +0200

    Fixed sidebar bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <AdminProductsPage></AdminProductsPage>
+        <AdminUserPage></AdminUserPage>
       </div>
     );
   }
